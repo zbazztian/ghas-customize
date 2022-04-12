c2549d1bcd2962ecd37296e0f55c90e74dad0b6b
Sebastian Bauersfeld <sebastian@semmle.com>
2022-04-12 19:17:07 +0700

    update

--- integrationtest/script.py
+++ integrationtest/script.py
@@ -1,4 +1,5 @@
 def customize(utils):
-  utils.copy2dir('FieldCustomizations.qll', 'qlpacks/codeql/java-all/*/')
-  utils.append('import.append', 'qlpacks/codeql/java-all/*/Customizations.qll')
+  pass
+  #utils.copy2dir('FieldCustomizations.qll', 'qlpacks/codeql/java-all/*/')
+  #utils.append('import.append', 'qlpacks/codeql/java-all/*/Customizations.qll')
   #utils.rebuild_packs('qlpacks/codeql/java-queries/[0-9]*.[0-9]*.[0-9]*/')
